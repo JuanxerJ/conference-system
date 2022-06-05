db72ba466a3b78515fb021a9349a74ce9e82c1c0
JuanxerJ <jjhenao.jar@gmail.com>
2022-06-04 22:47:35 -0500

    Build: add the class "Curso"

---
+++ src/cursos.ts
@@ -0,0 +1,10 @@
+export class Curso {
+  public name: string;
+  public code: string;
+  public url: string;
+  constructor(name: string, code: string, url: string) {
+    this.name = name;
+    this.code = code;
+    this.url = url;
+  }
+}
